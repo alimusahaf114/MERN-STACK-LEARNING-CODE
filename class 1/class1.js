62f63eae0b5422c0522dadb47107485425e51ce4
Musahaf Ali <musahafali43@gmail.com>
2026-06-28 12:33:13 +0500

    I have learned creating server in node.js

---
+++ class 1/class1.js
@@ -0,0 +1,6 @@
+// console.log("Hello world");
+// const fs = require('fs');
+// fs.writeFile("musahaf.txt" , "This is the file that I have always tried to build as a beginner developer" , (err) =>{
+//     if(err) console.log("There is error of :" , err);
+//     else console.log("No Error file executed successfully"); 
+// })
